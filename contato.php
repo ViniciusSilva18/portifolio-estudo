a56b37c323a4b24e06408225db5de97026d14068
Vinicius Musk <vinicius.rodrigues200015@gmail.com>
2021-12-08 19:23:55 -0300

    criar projeto

---
+++ contato.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifolio</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="fonteawesome.min.css">
+</head>
+
+<body>
+    <div class="menu">
+        <div class="menu-corpo">
+            
+            <a class="text-white" href="/">inicio</a>
+            <a class="text-white" href="/">contato</a>
+        </div>
+    </div>
+
+    <div class="bloco-contato">
+
+    </div>
+    <div class="menu">
+        <div class="menu-corpo">
+            
+            <a class="text-white" href="/">
+                <i class="fab fa-facebook"> </i>
+            </a>
+            <a  class="text-white" target="_blank" href="https://wa.me/5518997600702">
+                <i class="fab fa-whatsapp"> </i>
+            </a>
+
+            <a  class="text-white" href="/">
+                <i class="fab fa-instagram-square"> </i>
+            </a>
+
+            <a  class="text-white" href="mailto:viniciu.rodrigues200015@gmail.com">
+                <i class="fas fa-envelope-square"></i>
+            </a>
+
+            <a  class="text-white" href="tel:11994334690">
+                <i class="fas fa-phone"></i>
+            </a>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifolio</title>
+    <link rel="icon" type="image/png" href="/public/assets/images/favicon.png">
+    <link rel="stylesheet" href="/public/assets/css/estilo.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+
+<body>
+    <div class="menu">
+        <div class="menu-corpo">
+            
+            <a class="text-white" href="/">inicio</a>
+            <a class="text-white" href="/contato.php">contato</a>
+        </div>
+    </div>
+
+    <div class="bloco-pessoa">
+    <img class="imagem-pesso" src="/public/assets/images/pessoa.png" alt="pessoa" id="pessoa" >
+
+    </div>
+    <div class="menu">
+        <div class="menu-corpo">
+            
+            <a class="text-white" href="/">
+                <i class="fab fa-facebook"> </i>
+            </a>
+            <a  class="text-white" target="_blank" href="https://wa.me/5518997600702">
+                <i class="fab fa-whatsapp"> </i>
+            </a>
+
+            <a  class="text-white" href="/">
+                <i class="fab fa-instagram"> </i>
+            </a>
+
+            <a  class="text-white" href="mailto:viniciu.rodrigues200015@gmail.com">
+                <i class="fas fa-envelope-square"></i>
+            </a>
+
+            <a  class="text-white" href="tel:11994334690">
+                <i class="fas fa-phone"></i>
+            </a>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
